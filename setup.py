f952ff999862713094ff1874d071e188a9246a12
mitsuhiko <devnull@localhost>
2007-09-16 20:33:36 +0200

    added a setup file. wohoo

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='CleverCSS',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    version='0.1',
+    url='http://sandbox.pocoo.org/clevercss/',
+    py_modules=['clevercss'],
+    description='funky css preprocessor dammit',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python'
+    ]
+)
