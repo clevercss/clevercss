6adafebf458865eb3d13cc74e0099d594ea3760e
lericson <ludvig@lericson.se>
2009-10-26 15:39:57 +0100

    Fix an apparent typo of the name EvalException.

--- clevercss.py
+++ clevercss.py
@@ -1073,8 +1073,8 @@ class RGB(Expr):
                                     'rgb() literal only accept numbers and '
                                     'percentages.')
             if value < 0 or value > 255:
-                raise EvalError(self.lineno, 'rgb components must be in '
-                                'the range 0 to 255.')
+                raise EvalException(self.lineno, 'rgb components must be in '
+                                    'the range 0 to 255.')
             args.append(value)
         return Color(args, lineno=self.lineno)
 
