a03893a050b17c83e9697ece3efa268a057d7970
Matt Chisholm <matt@theory.org>
2011-04-20 22:39:19 +0200

    fix setup.py to work with new package, script name, and dependency on cssutils

--- setup.py
+++ setup.py
@@ -15,7 +15,9 @@ setup(
     version='0.2',
     url='http://sandbox.pocoo.org/clevercss/',
     download_url='http://github.com/dziegler/clevercss/tree',
-    py_modules=['clevercss'],
+    packages=['clevercss'],
+    scripts=['bin/ccss'],
+    install_requires=['cssutils'],
     description='python inspired sass-like css preprocessor',
     long_description=readme_text,
     classifiers=[
