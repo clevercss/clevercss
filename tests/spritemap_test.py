41bd7978cc14a9dc925dd924a4e4fc3fda087858
Petr Viktorin <encukou@gmail.com>
2011-10-16 03:27:12 +0300

    Fix test: Simple strings in URLs don't need quoting

--- tests/spritemap_test.py
+++ tests/spritemap_test.py
@@ -15,14 +15,14 @@ class SpriteMapTestCase(TestCase):
             correct)
 
 correct = '''body {
-  background-image: url('big.png') -0px -0px;
+  background-image: url(big.png) -0px -0px;
   width: 20px;
   height: 20px;
 }
 
 body div.other,
 body .some {
-  background-image: url('big.png') -0px -20px;
+  background-image: url(big.png) -0px -20px;
 }'''
 def all_tests():
     return unittest.TestSuite(case.toSuite() for case in [SpriteMapTestCase])
